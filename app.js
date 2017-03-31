b02f000b6f6a62aeffc7be075e1bbaa8a50d3ab2
Parteek Makkar <parteek_makkar@ymail.com>
2017-03-31 15:16:52 -0700

    Changes for File Upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var images = require('./routes/images');
 
 var app = express();
 
@@ -26,10 +27,12 @@ app.use(require('node-sass-middleware')({
   indentedSyntax: true,
   sourceMap: true
 }));
+app.use('/bower_components',express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/images', images);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
