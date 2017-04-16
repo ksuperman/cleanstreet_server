63e67825e09dbc8b76ae89ff853c855c7a677ed3
lazylad91 <parteek_makkar@ymail.com>
2017-04-16 15:03:54 -0700

    Changing the port to 8080 for server

--- routes/images.js
+++ routes/images.js
@@ -1,10 +1,30 @@
 var express = require('express');
+var producer = require('./producer');
+var message = require('./message');
 var router = express.Router();
 
-/* Send Image to server. */
+/* Send Image to kafka topic. */
 router.post('/fileUpload', function(req, res, next) {
-    console.log(req);
-    res.send(req.body);
+    console.log(req.body);
+    var kafkamessage = [];
+    try {
+        if (producer.producer.isReady) {
+            kafkamessage.push(message.createMessage('keyed',"image", JSON.stringify(req.body)));
+            kafkamessage.push(message.createMessage("simple","this is simple message",""));
+            producer.sendMessage("test",kafkamessage,0,0,function(result){
+                res.send(result);
+            });
+        }
+        else{
+            res.send("producer is not ready");
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.send(error);
+    }
+
+
 });
 
 module.exports = router;
---
+++ routes/producer.js
@@ -0,0 +1,76 @@
+/**
+ * Created by lazylad91 on 3/31/17.
+ */
+
+var kafka = require('kafka-node');
+var Producer = kafka.HighLevelProducer;
+var Client = kafka.Client;
+var Promise = require('promise');
+var client = new Client('localhost:2181');
+
+//var argv = require('optimist').argv;
+var producer = new Producer(client, { requireAcks: 1,  partitionerType: 2});
+
+producer.isReady = false;
+
+producer.on('ready', function () {
+    producer.isReady = true;
+    console.log("Producer is ready now");
+});
+
+producer.on('error', function (err) {
+    console.log('error', err);
+});
+
+
+/**
+    Function for sending message to
+    Arguements
+    topic - topic name
+    messageArray - array of messages to be push to topic
+    partition - number of partition
+    attribute - attribute
+    callback - callback function which will be called with the result
+
+ */
+var sendMessage = function(topic, messageArray, partition, attribute, callback){
+    console.log("send message starting");
+    var promise = new Promise(function(resolve,reject){
+            var payload = [];
+            try {
+                if (Array.isArray(topic)) {
+                    for (var i = 0; i < topic.length; i++) {
+                        payload.push({
+                            topic: topic[i],
+                           // partition: partition,
+                            messages: messageArray,
+                            attributes: attribute
+                        });
+                    }
+                }
+                else {
+                    payload.push({topic: topic,  messages: messageArray, attributes: attribute});
+                }
+
+                resolve(payload);
+            }
+            catch(error){
+                reject(error);
+            }
+            });
+
+    promise.then(function(payload){
+        producer.send(payload, function (err, result) {
+            console.log(err || result);
+            console.log("i am here");
+            console.log(result);
+            callback(err || result);
+            //process.exit();
+        });
+    },function(error){
+        console.log(error);
+    });
+
+}
+
+module.exports = {producer: producer, sendMessage: sendMessage};
