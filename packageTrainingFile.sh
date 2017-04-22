6822b5894f2aa262917a0b41e607bc4ded18fd86
Rakshith Koravadi Hatwar <rakshithk@live.com>
2017-04-22 16:07:18 -0700

    Handling for Bulk Export

--- packageTrainingFile.sh
+++ packageTrainingFile.sh
@@ -10,21 +10,22 @@ mkdir data/annotations
 mkdir data/train2014
 mkdir data/val2014
 
-cp ./temp/instances_train2014.json data/annotations/instances_train2014.json
-cp ./temp/instances_val2014.json data/annotations/instances_val2014.json
 
-cp -a ./images/img/training/. ./data/train2014
-cp -a ./images/img/validation/. ./data/val2014
+cp -a ./temp/instances_val2014.json ./data/annotations/instances_val2014.json
+cp -a ./temp/instances_train2014.json ./data/annotations/instances_train2014.json
 
-rm data.tar.xz
+cp -a ./images/img/training/. ./data/train2014/
+cp -a ./images/img/validation/. ./data/val2014/
+
+# rm data.tar.xz
 
 if [ "$1" == "sendToServer" ]; then echo $1; fi
 
 tar cJf data.tar.xz ./data
 
-#rm -r data
+rm -r ./data
 
-#rm -r temp
+rm -r ./temp
 
 echo "File Package Complete"
 echo "============================"
