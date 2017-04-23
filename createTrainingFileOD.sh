06fc85ae6268ca4dd14a87110e55bc826db60d47
chandnib <chandnibalchandani@gmail.com>
2017-04-23 16:39:32 -0700

    modifications for object detection

--- createTrainingFileOD.sh
+++ createTrainingFileOD.sh
@@ -9,8 +9,8 @@ mkdir temp
 mongoexport --host $1 --db clean_street --collection imageods --jsonArray --query '{"status": "N", "image_type":"training"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/imageods.json
 mongoexport --host $1 --db clean_street --collection imageods --jsonArray --query '{"status": "N", "image_type":"validate"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validationImageods.json
 
-mongoexport --host $1 --db clean_street --collection imageannotations --jsonArray --query '{"image_type":"training"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/annotationsod.json
-mongoexport --host $1 --db clean_street --collection imageannotations --jsonArray --query '{"image_type":"validate"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validateAnnotationsod.json
+mongoexport --host $1 --db clean_street --collection imageannotationods --jsonArray --query '{"image_type":"training"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/annotationsod.json
+mongoexport --host $1 --db clean_street --collection imageannotationods --jsonArray --query '{"image_type":"validate"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validateAnnotationsod.json
 
 echo "Export Complete"
 echo "============================"
--- packageTrainingFileOD.sh
+++ packageTrainingFileOD.sh
@@ -13,12 +13,12 @@ mkdir data/val2014
 cp -a ./temp/instances_val2014.json ./data/annotations/instances_val2014.json
 cp -a ./temp/instances_train2014.json ./data/annotations/instances_train2014.json
 
-cp -a ./images/img/training/. ./data/train2014/
-cp -a ./images/img/validation/. ./data/val2014/
+cp -a ./images/obj/training/. ./data/train2014/
+cp -a ./images/obj/validation/. ./data/val2014/
 
 # rm data_od.tar.xz
 
-if [ "$1" == "sendToServer" ]; then echo $1; fi
+#if [ "$1" == "sendToServer" ]; then echo $1; fi
 
 tar cJf data_od.tar.xz ./data
 
