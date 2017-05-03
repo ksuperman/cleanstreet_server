87f183262a2a943fe839620c0d80da55806563ab
Rakshith Koravadi Hatwar <rakshithk@live.com>
2017-05-03 01:05:20 -0700

    Fix for Training Data Generation Script

--- createTrainingFile.sh
+++ createTrainingFile.sh
@@ -6,8 +6,8 @@ export PATH=/opt/mongodb/bin/:/usr/local/cuda-8.0/bin:/usr/local/cuda/bin:/home/
 rm -r temp
 mkdir temp
 
-mongoexport --host $1 --db clean_street --collection images --jsonArray --query '{"status": "N", "image_type":"training"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/images.json
-mongoexport --host $1 --db clean_street --collection images --jsonArray --query '{"status": "N", "image_type":"validate"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validationImages.json
+mongoexport --host $1 --db clean_street --collection images --sort '{"date_captured": -1}' --jsonArray --query '{"status": "N", "image_type":"training"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/images.json
+mongoexport --host $1 --db clean_street --collection images --sort '{"date_captured": -1}' --jsonArray --query '{"status": "N", "image_type":"validate"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validationImages.json
 
 mongoexport --host $1 --db clean_street --collection imageannotations --jsonArray --query '{"image_type":"training"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/annotations.json
 mongoexport --host $1 --db clean_street --collection imageannotations --jsonArray --query '{"image_type":"validate"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validateAnnotations.json
--- createTrainingFileOD.sh
+++ createTrainingFileOD.sh
@@ -6,8 +6,8 @@ export PATH=/opt/mongodb/bin/:/usr/local/cuda-8.0/bin:/usr/local/cuda/bin:/home/
 rm -r temp
 mkdir temp
 
-mongoexport --host $1 --db clean_street --collection imageods --jsonArray --query '{"status": "N", "image_type":"training"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/imageods.json
-mongoexport --host $1 --db clean_street --collection imageods --jsonArray --query '{"status": "N", "image_type":"validate"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validationImageods.json
+mongoexport --host $1 --db clean_street --collection imageods --sort '{"date_captured": -1}' --jsonArray --query '{"status": "N", "image_type":"training"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/imageods.json
+mongoexport --host $1 --db clean_street --collection imageods --sort '{"date_captured": -1}' --jsonArray --query '{"status": "N", "image_type":"validate"}' --fields coco_url,date_captured,file_name,flickr_url,height,width,id,license | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validationImageods.json
 
 mongoexport --host $1 --db clean_street --collection imageannotationods --jsonArray --query '{"image_type":"training"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/annotationsod.json
 mongoexport --host $1 --db clean_street --collection imageannotationods --jsonArray --query '{"image_type":"validate"}' | sed '/"_id":/s/"_id":[^,]*,//g' >> ./temp/validateAnnotationsod.json
